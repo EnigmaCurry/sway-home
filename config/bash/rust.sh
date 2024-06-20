a3298363c22280d38314ece2a2bd44b46e5e1b01
EnigmaCurry <ryan@enigmacurry.com>
2024-06-20 14:01:51 -0600

    rust bootstrap

--- config/bash/rust.sh
+++ config/bash/rust.sh
@@ -4,11 +4,12 @@ if [[ -f "$HOME/.cargo/env" ]]; then
     unset -f fedora-bootstrap-rust
     source "$HOME/.cargo/env"
 else
-    ## On a new machine, you should run bootstrap-rust.
-    bootstrap-rust() {
-        sudo dnf install -y rustup
-        rustup-init -y --no-modify-path
+    ## On a new machine, you should run rust-bootsrap
+    rust-bootstrap() {
+        bash <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)\
+             -y --no-modify-path
         source "$HOME/.cargo/env"
         rustup component add rust-analyzer
+        unset -f rust-bootstrap
     }
 fi
