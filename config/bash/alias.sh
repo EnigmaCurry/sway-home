3e2d429aa71c31a46290a409a1f4f7e27ea20fcc
EnigmaCurry <ryan@enigmacurry.com>
2024-09-15 15:03:33 -0600

    alias run='just run'

--- config/bash/alias.sh
+++ config/bash/alias.sh
@@ -5,5 +5,6 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias bb='rlwrap bb'
 alias ec="${HOME}/git/vendor/enigmacurry/emacs/ec"
+alias run='just run'
 
 vars() { set -o posix; set | cut -d= -f1 | column; }
