1b1afa3fb5fa8f595c2ab0e2277116e5cb78549b
EnigmaCurry <ryan@enigmacurry.com>
2024-09-24 23:06:18 -0600

    fix indentation

--- config/bash/completion.sh
+++ config/bash/completion.sh
@@ -6,10 +6,10 @@
 #### add the following lines into your ~/.bashrc ::
 if [[ -d ${HOME}/git/vendor/enigmacurry/d.rymcg.tech ]]; then
     export PATH=${PATH}:${HOME}/git/vendor/enigmacurry/d.rymcg.tech/_scripts/user
-   eval "$(d.rymcg.tech completion bash)"
-   ## Example project alias: creates a shorter command used just for the Traefik project:
-   __d.rymcg.tech_cli_alias d
-   __d.rymcg.tech_project_alias traefik
+    eval "$(d.rymcg.tech completion bash)"
+    ## Example project alias: creates a shorter command used just for the Traefik project:
+    __d.rymcg.tech_cli_alias d
+    __d.rymcg.tech_project_alias traefik
 fi
 
 # https://github.com/casey/just
