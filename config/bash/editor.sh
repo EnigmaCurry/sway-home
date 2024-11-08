1378e14e5dbe8d5961c1bbfe9db8163da50e59f3
EnigmaCurry <ryan@enigmacurry.com>
2024-11-08 10:44:50 -0700

    editor

--- config/bash/editor.sh
+++ config/bash/editor.sh
@@ -1,1 +1,14 @@
-export EDITOR="emacsclient -q"
+#!/bin/bash
+
+# Hardcode your preferred editor here:
+preferred_editor="emacsclient   -q"
+
+if [ "$0" != "$BASH_SOURCE" ]; then
+    # Sourcing this file..
+    export EDITOR="${HOME}/.config/bash/editor.sh"
+    export VISUAL=${EDITOR}
+else
+    # Running this file as a script:
+    notify-send "Opening file" "Opening '$1' in $EDITOR_COMMAND"
+    exec $preferred_editor "$1"
+fi
