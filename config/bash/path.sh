9f1fdaba9846b441e734e44a147ee6d3b49f06d6
EnigmaCurry <ryan@enigmacurry.com>
2024-11-12 11:05:32 -0700

    go path

--- config/bash/path.sh
+++ config/bash/path.sh
@@ -1,5 +1,8 @@
 # Add ~/bin to your PATH for user programs:
 PATH=${PATH}:${HOME}/bin
 
+# Add go path:
+PATH=${HOME}/go/bin:/usr/local/go/bin:${PATH}
+
 # Load cargo path if installed:
 test -f $HOME/.cargo/env && source "$HOME/.cargo/env"
