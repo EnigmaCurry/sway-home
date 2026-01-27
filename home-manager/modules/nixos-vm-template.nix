a24a1921dfccf48819e2b42c4a67a6e6ccd182da
EnigmaCurry <ryan@enigmacurry.com>
2026-01-27 22:12:16 +0000

    Create writable env file for nixos-vm-template

--- home-manager/modules/nixos-vm-template.nix
+++ home-manager/modules/nixos-vm-template.nix
@@ -2,7 +2,18 @@
 
 let
   nixosVmTemplateRepo = inputs.nixos-vm-template;
+  envFile = "${config.xdg.configHome}/nixos-vm-template/env";
+  defaultEnv = ''
+    OUTPUT_DIR=$HOME/.local/share/nixos-vm-template
+  '';
 in
 {
   home.file."nixos-vm-template".source = nixosVmTemplateRepo;
-}
+
+  home.activation.nixos-vm-template-env = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -f "${envFile}" ]; then
+      mkdir -p "$(dirname "${envFile}")"
+      cat > "${envFile}" << 'EOF'
+${defaultEnv}EOF
+    fi
+  '';
