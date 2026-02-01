367907c41ecdef02fd3bddf51d8dfff293bbcdee
EnigmaCurry <ryan@enigmacurry.com>
2026-02-01 18:02:34 +0000

    Add midi_reset script to send All Notes Off

--- home-manager/modules/fluidsynth.nix
+++ home-manager/modules/fluidsynth.nix
@@ -1,6 +1,38 @@
 # FluidSynth and soundfonts module
 { config, pkgs, lib, ... }:
 
+let
+  # Script to send All Notes Off to fluidsynth (or specified port)
+  # Sends CC 120 (All Sound Off) and CC 123 (All Notes Off) on all 16 channels
+  midiReset = pkgs.writeShellScriptBin "midi_reset" ''
+    PORT="''${1:-128:0}"
+    MIDI_FILE=$(mktemp --suffix=.mid)
+    trap "rm -f $MIDI_FILE" EXIT
+    # Pre-computed MIDI file: header + CC 120/123 on all 16 channels + end of track
+    printf '\x4D\x54\x68\x64\x00\x00\x00\x06\x00\x00\x00\x01\x00\x60' > "$MIDI_FILE"
+    printf '\x4D\x54\x72\x6B\x00\x00\x00\x86' >> "$MIDI_FILE"
+    # Channel 0-15: CC 120 (All Sound Off) and CC 123 (All Notes Off)
+    printf '\x00\xB0\x78\x00\x00\xB0\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xB1\x78\x00\x00\xB1\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xB2\x78\x00\x00\xB2\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xB3\x78\x00\x00\xB3\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xB4\x78\x00\x00\xB4\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xB5\x78\x00\x00\xB5\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xB6\x78\x00\x00\xB6\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xB7\x78\x00\x00\xB7\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xB8\x78\x00\x00\xB8\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xB9\x78\x00\x00\xB9\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xBA\x78\x00\x00\xBA\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xBB\x78\x00\x00\xBB\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xBC\x78\x00\x00\xBC\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xBD\x78\x00\x00\xBD\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xBE\x78\x00\x00\xBE\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xBF\x78\x00\x00\xBF\x7B\x00' >> "$MIDI_FILE"
+    printf '\x00\xFF\x2F\x00' >> "$MIDI_FILE"
+    ${pkgs.alsa-utils}/bin/aplaymidi -p "$PORT" "$MIDI_FILE"
+    echo "Sent All Notes Off to $PORT"
+  '';
+in
 {
   home.packages = with pkgs; [
     fluidsynth
@@ -8,6 +40,7 @@
     soundfont-generaluser
     soundfont-ydp-grand
     x42-gmsynth
+    midiReset
   ];
 
   # Create ~/soundfonts with symlinks to nix store soundfonts
