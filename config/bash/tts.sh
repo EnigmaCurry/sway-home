9b4377186809825e30c41af0ff0d1f679ffc8a95
EnigmaCurry <ryan@enigmacurry.com>
2026-04-18 06:33:27 +0000

    Remove say wrapper, rely on standalone bin/say binary

--- config/bash/tts.sh
+++ config/bash/tts.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 ## Text to speech via pocket-tts server
-
-say() {
-    "$(dirname "${BASH_SOURCE[0]}")/../bin/say" "$@"
-}
+# The actual `say` binary is at ~/bin/say (symlinked from sway-home/bin/say).
