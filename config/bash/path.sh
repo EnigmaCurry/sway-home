8c219f248514caabb69b94c1979de42ce5a57c91
EnigmaCurry <ryan@enigmacurry.com>
2026-05-15 07:11:25 +0000

    Prepend ~/bin and ~/.local/bin to PATH for higher priority

--- config/bash/path.sh
+++ config/bash/path.sh
@@ -1,5 +1,5 @@
 # Add ~/bin to your PATH for user programs:
-PATH=${PATH}:${HOME}/bin:${HOME}/.local/bin
+PATH=${HOME}/bin:${HOME}/.local/bin:${PATH}
 
 # Add go path:
 PATH=${HOME}/go/bin:/usr/local/go/bin:${PATH}
