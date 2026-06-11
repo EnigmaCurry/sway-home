1de66769074e0bbcee31967df7f25409068a53bf
EnigmaCurry <ryan@enigmacurry.com>
2026-06-11 15:42:35 +0000

    upgrade to nixos-26.05

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,11 +2,11 @@
   description = "Home Manager configuration for sway-home";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
